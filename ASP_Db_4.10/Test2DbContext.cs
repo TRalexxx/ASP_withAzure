aecd1e3b42c96d93f9dbb1c49d22c0d867db478f
TRalexxx <110291348+TRalexxx@users.noreply.github.com>
2024-01-27 13:20:39 +0200

    Add Test2DbContext and User classes, update .gitignore
    This commit introduces the Test2DbContext and User model classes to the ASP_Db_4.10 project. It also updates .gitignore file to prevent unwanted files/directories from being tracked within the .idea directory. Lastly, encodings.xml, indexLayout.xml, and vcs.xml files associated with the .idea directory also found their places in the project.

---
+++ ASP_Db_4.10/Test2DbContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP_Db_4._10;
+
+public partial class Test2DbContext : DbContext
+{
+    public Test2DbContext()
+    {
+    }
+
+    public Test2DbContext(DbContextOptions<Test2DbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(x=>x.Id);
+
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(60)
+                .IsUnicode(false);
+            entity.Property(e => e.Id).ValueGeneratedOnAdd();
+            entity.Property(e => e.LastName)
+                .HasMaxLength(60)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ ASP_Db_4.10/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ASP_Db_4._10;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+}
